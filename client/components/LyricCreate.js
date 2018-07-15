b4d23b87be2d7cbab01db1184f2cf64854c7c53d
MEA\Aviral.Yadav <aviral.yadav@in.ey.com>
2018-07-15 12:32:17 +0530

    Added Lyric and Likes per Lyric

--- client/components/LyricCreate.js
+++ client/components/LyricCreate.js
@@ -1,29 +1,29 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
-import {graphql} from 'react-apollo';
+import { graphql } from 'react-apollo';
 import query from '../queries/fetchSongs';
 
 class LyricCreate extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             content: ''
         }
     }
-    onSubmit (event) {
+    onSubmit(event) {
         event.preventDefault();
         this.props.mutate({
             variables: {
                 songId: this.props.songId,
                 content: this.state.content
             }
-        }).then(()=>this.setState({content: ''}));
+        }).then(() => this.setState({ content: '' }));
     }
     render() {
         return (
             <form onSubmit={this.onSubmit.bind(this)}>
                 <label>Add Lyric</label>
-                <input value={this.state.content} onChange={event=>this.setState({content: event.target.value})} />
+                <input value={this.state.content} onChange={event => this.setState({ content: event.target.value })} />
             </form>
         );
     }
@@ -36,6 +36,7 @@ const mutation = gql`
         lyrics {
             id
             content
+            likes
         }
         }
     }
--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 class LyricList extends Component {
-    onLick (id) {
-        console.log(id);
+    onLick(id, likes) {
+        this.props.mutate({
+            variables: { id },
+            optimisticResponse: {
+                __typename: 'Mutation',
+                likeLyric: {
+                    id,
+                    likes: likes + 1,
+                    __typename: 'LyricType'
+                }
+            }
+        });
+
     }
-    renderLyrics () {
-        return this.props.lyrics.map(({id, content})=> {
-            return <li className="collection-item" key={id}>{content} <i className="material-icons" onClick={(id)=>this.onLick(id)}>thumb_up</i></li>;
+    renderLyrics() {
+        return this.props.lyrics.map(({ id, content, likes }) => {
+            return <li className="collection-item" key={id}>
+                {content}
+                <div className="vote-box">
+                    <i className="material-icons" onClick={() => this.onLick(id, likes)}>thumb_up</i>
+                    {likes}
+                </div>
+            </li>;
         });
     }
     render() {
@@ -18,4 +37,13 @@ class LyricList extends Component {
     }
 }
 
-export default LyricList;
+const mutation = gql`
+    mutation LyricLike($id: ID) {
+        likeLyric(id: $id) {
+        id
+        likes
+        }
+    }
+`;
+
+export default graphql(mutation)(LyricList);
